82a4ffc19a040b1d14e40fd27edc2a36da68a817
Muhammad Ahmed <ahmedmughal3182@gmail.com>
2024-03-26 10:34:58 +0500

    first commit

---
+++ assets/demoData.js
@@ -0,0 +1,19 @@
+const demoData = [
+
+    {
+        title: "Watch ultra",
+        price: 500,
+        category: "Wear",
+        tags: ["New", "Apple", '2024'],
+        onsale: true,
+    },
+    {
+        title: "Watch 9",
+        price: 3500,
+        category: "Wear",
+        tags: ["New", "Apple", '2024'],
+        onsale: true,
+    }
+]
+
+export default demoData;
